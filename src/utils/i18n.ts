c9fe0976039f8fd4a48c3f109497dfe31d0cca38
realbro <realbro.noh@gmail.com>
2024-08-13 09:42:56 +0000

    use translation json file

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -1,29 +1,22 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import { Language } from "../types/i18n";
+import Backend from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
 
-const resources = {
-  en: {
-    translation: {
-      welcome: "Hello, I'm RealbroNoh",
-      description: "this is my resume",
+i18n
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: Language.ENGLISH,
+    debug: true,
+    backend: {
+      loadPath: "/locales/{{lng}}.json",
     },
-  },
-  ko: {
-    translation: {
-      welcome: "안녕하세요? 저는 노진형입니다.",
-      description: "이력서입니다.",
+    interpolation: {
+      escapeValue: false, // react already safes from xss
     },
-  },
-};
-
-i18n.use(initReactI18next).init({
-  resources,
-  lng: Language.ENGLISH, // default language
-  fallbackLng: Language.ENGLISH,
-  interpolation: {
-    escapeValue: false, // react already safes from xss
-  },
-});
+  });
 
 export default i18n;
